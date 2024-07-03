83459b637c1d3c6f2bcef9d31268a6803652e165
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-07-03 15:56:24 +0200

    feat(docs): add more blobs to hero page

---
+++ packages/docs/src/components/Blobs/Blob6.tsx
@@ -0,0 +1,23 @@
+interface Blob6Props {
+  className: string;
+}
+
+export const Blob6: React.FC<Blob6Props> = ({ className }: Blob6Props) => {
+  return (
+    <svg
+      width="629"
+      height="442"
+      viewBox="0 0 629 442"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M627.334 201.467C637.641 289.262 574.445 375.255 499.593 422.28C434.799 462.986 354.998 427.225 278.78 420.44C213.938 414.667 145.179 426.495 94.0683 386.178C37.8186 341.807 -3.75435 272.94 1.18401 201.467C5.93124 132.76 62.056 80.8771 117.944 40.6307C164.485 7.11436 221.473 3.88213 278.78 1.56121C339.81 -0.910496 400.209 -3.33498 453.193 27.0531C527.279 69.5434 617.376 116.644 627.334 201.467Z"
+        fill="#C4C4C4"
+      />
+    </svg>
+  );
+};
