99e3c6ede99755b9cd71a7b0687ddbd50ee28799
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-07-03 10:03:45 +0200

    feat(docs): add animated blobs to hero page

---
+++ packages/docs/src/components/Blobs/Blob4.tsx
@@ -0,0 +1,22 @@
+interface Blob4Props {
+  className: string;
+}
+
+export const Blob4: React.FC<Blob4Props> = ({ className }: Blob4Props) => {
+  return (
+    <svg
+      width="313"
+      height="538"
+      viewBox="0 0 313 538"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M103.84 10.203C171.878 -24.2097 269.322 33.9739 303.824 101.967C336.223 165.815 269.958 232.672 251.885 301.951C241.282 342.594 243.448 383.636 221.072 419.182C190.372 467.951 160.879 545.34 103.84 537.133C44.9921 528.665 39.8563 444.192 16.1016 389.689C3.78325 361.426 -3.06043 332.467 1.33689 301.951C5.05759 276.131 29.5559 260.926 38.2031 236.314C65.2474 159.34 31.0358 47.0264 103.84 10.203Z"
+      />
+    </svg>
+  );
+};
