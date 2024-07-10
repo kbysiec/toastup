14e78c7fa0f77ffbd44e40b6a0fdee39509473f2
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-07-10 21:36:01 +0200

    feat(react): change public api structure

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,17 +1,129 @@
 import "@toastup/core/style";
 
-import { add } from "@toastup/core";
+import {
+  actionType,
+  add,
+  animationElementSelector,
+  animationType,
+  bounceHorizontallyIn,
+  bounceHorizontallyOut,
+  cssClassNames,
+  displayOrder,
+  events,
+  fadeIn,
+  fadeOut,
+  getCoalesced,
+  getOuter,
+  getTransformOtherThan,
+  groupBy,
+  isFunction,
+  isHTMLElement,
+  measureType,
+  pause,
+  position,
+  remove,
+  removeAll,
+  singleBounceHorizontallyIn,
+  singleBounceHorizontallyOut,
+  singleBounceVerticallyIn,
+  singleBounceVerticallyOut,
+  sleep,
+  slideHorizontallyIn,
+  slideHorizontallyOut,
+  slideHorizontalWithFadeInBody,
+  slideVerticallyIn,
+  slideVerticallyOut,
+  slideVerticallyWithFadeInBody,
+  theme,
+  type,
+  unpause,
+  uuid,
+  zoomIn,
+  zoomInBody,
+  zoomOut,
+  zoomWithBounceIn,
+  zoomWithBounceOut,
+} from "@toastup/core";
+import { Body } from "./components/Body";
+import { Container } from "./components/Container";
+import { Content } from "./components/Content";
+import { HideButton } from "./components/HideButton";
+import { Icon } from "./components/Icon";
+import { ProgressBar } from "./components/ProgressBar";
+import { Toast } from "./components/Toast";
+import { Toaster } from "./components/Toaster";
 import { ReactToastConfig } from "./reactTypes";
-const reactAdd = add<ReactToastConfig>;
 
-export * from "@toastup/core";
-export * from "./components/Body";
-export * from "./components/Container";
-export * from "./components/Content";
-export * from "./components/HideButton";
-export * from "./components/Icon";
-export * from "./components/ProgressBar";
-export * from "./components/Toast";
-export * from "./components/Toaster";
+import type * as types from "@toastup/core";
+
+import { reactEvents } from "./constants";
 export * from "./reactTypes";
-export { reactAdd as add };
+export { types };
+
+const toast = {
+  add: add<ReactToastConfig>,
+  remove,
+  removeAll,
+  pause,
+  unpause,
+};
+
+const animation = {
+  body: {
+    slideHorizontalWithFadeInBody,
+    slideVerticallyWithFadeInBody,
+    zoomInBody,
+  },
+  in: {
+    fadeIn,
+    zoomIn,
+    zoomWithBounceIn,
+    slideVerticallyIn,
+    slideHorizontallyIn,
+    bounceHorizontallyIn,
+    singleBounceHorizontallyIn,
+    singleBounceVerticallyIn,
+  },
+  out: {
+    fadeOut,
+    zoomOut,
+    zoomWithBounceOut,
+    slideVerticallyOut,
+    slideHorizontallyOut,
+    bounceHorizontallyOut,
+    singleBounceHorizontallyOut,
+    singleBounceVerticallyOut,
+  },
+};
+
+export {
+  actionType,
+  animation,
+  animationElementSelector,
+  animationType,
+  Body,
+  Container,
+  Content,
+  cssClassNames,
+  displayOrder,
+  events,
+  getCoalesced,
+  getOuter,
+  getTransformOtherThan,
+  groupBy,
+  HideButton,
+  Icon,
+  isFunction,
+  isHTMLElement,
+  measureType,
+  position,
+  ProgressBar,
+  reactEvents,
+  sleep,
+  theme,
+  toast,
+  Toast,
+  Toaster,
+  type,
+  uuid,
+};
