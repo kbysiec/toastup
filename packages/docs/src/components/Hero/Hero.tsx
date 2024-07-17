5c64bfeab67e970ade4b11d9d7d45581c2186bbc
Kamil Bysiec <kamil.bysiec.agileplayers@gmail.com>
2024-07-17 21:24:07 +0200

    fix(docs): improve hero page styling

--- packages/docs/src/components/Hero/Hero.tsx
+++ packages/docs/src/components/Hero/Hero.tsx
@@ -162,191 +162,208 @@ add(config);`;
     <>
       <div className={styles.container}>
         <div className={styles.basicSettings}>
-          <div className={styles.row}>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="autoHide">
-                Autohide time
-              </label>
-              <input
-                className={styles.input}
-                name="autoHide"
-                type="number"
-                value={autoHide}
-                onChange={e => setAutoHide(parseInt(e.target.value))}
-              />
+          <div className={styles.twoRows}>
+            <div className={styles.row}>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="autoHide">
+                  Autohide time
+                </label>
+                <input
+                  className={styles.input}
+                  name="autoHide"
+                  type="number"
+                  value={autoHide}
+                  onChange={e => setAutoHide(parseInt(e.target.value))}
+                />
+              </div>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="toastType">
+                  Type
+                </label>
+                <select
+                  name="toastType"
+                  className={styles.select}
+                  value={availableType}
+                  onChange={e => setAvailableType(e.target.value as ToastType)}
+                >
+                  {Object.keys(type).map(key => (
+                    <option key={key} value={type[key as ToastType]}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="toastType">
-                Type
-              </label>
-              <select
-                name="toastType"
-                className={styles.select}
-                value={availableType}
-                onChange={e => setAvailableType(e.target.value as ToastType)}
-              >
-                {Object.keys(type).map(key => (
-                  <option key={key} value={type[key as ToastType]}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="toastPosition">
-                Position
-              </label>
-              <select
-                name="toastPosition"
-                className={styles.select}
-                value={availablePosition}
-                onChange={e =>
-                  setAvailablePosition(e.target.value as ToastPosition)
-                }
-              >
-                {Object.keys(position).map(key => (
-                  <option key={key} value={position[key as ToastPositionKey]}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="displayOrder">
-                Display order
-              </label>
-              <select
-                name="displayOrder"
-                className={styles.select}
-                value={order}
-                onChange={e => setOrder(e.target.value as DisplayOrder)}
-              >
-                {Object.keys(displayOrder).map(key => (
-                  <option key={key} value={displayOrder[key as DisplayOrder]}>
-                    {key}
-                  </option>
-                ))}
-              </select>
+            <div className={styles.row}>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="toastPosition">
+                  Position
+                </label>
+                <select
+                  name="toastPosition"
+                  className={styles.select}
+                  value={availablePosition}
+                  onChange={e =>
+                    setAvailablePosition(e.target.value as ToastPosition)
+                  }
+                >
+                  {Object.keys(position).map(key => (
+                    <option key={key} value={position[key as ToastPositionKey]}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="displayOrder">
+                  Display order
+                </label>
+                <select
+                  name="displayOrder"
+                  className={styles.select}
+                  value={order}
+                  onChange={e => setOrder(e.target.value as DisplayOrder)}
+                >
+                  {Object.keys(displayOrder).map(key => (
+                    <option key={key} value={displayOrder[key as DisplayOrder]}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
-          <div className={styles.row}>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="theme">
-                Theme
-              </label>
-              <select
-                name="theme"
-                className={styles.select}
-                value={availableTheme}
-                onChange={e => setAvailableTheme(e.target.value as ToastTheme)}
-              >
-                {Object.keys(theme).map(key => (
-                  <option key={key} value={theme[key as ToastThemeKey]}>
-                    {key}
-                  </option>
-                ))}
-              </select>
+          <div className={styles.twoRows}>
+            <div className={styles.row}>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="theme">
+                  Theme
+                </label>
+                <select
+                  name="theme"
+                  className={styles.select}
+                  value={availableTheme}
+                  onChange={e =>
+                    setAvailableTheme(e.target.value as ToastTheme)
+                  }
+                >
+                  {Object.keys(theme).map(key => (
+                    <option key={key} value={theme[key as ToastThemeKey]}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="inAnimation">
+                  In animation
+                </label>
+                <select
+                  name="inAnimation"
+                  className={styles.select}
+                  value={inAnimationName}
+                  onChange={e =>
+                    setInAnimationName(e.target.value as InAnimation)
+                  }
+                >
+                  {Object.keys(inAnimation).map(key => (
+                    <option key={key} value={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="inAnimation">
-                In animation
-              </label>
-              <select
-                name="inAnimation"
-                className={styles.select}
-                value={inAnimationName}
-                onChange={e =>
-                  setInAnimationName(e.target.value as InAnimation)
-                }
-              >
-                {Object.keys(inAnimation).map(key => (
-                  <option key={key} value={key}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="outAnimation">
-                Out animation
-              </label>
-              <select
-                name="outAnimation"
-                className={styles.select}
-                value={outAnimationName}
-                onChange={e =>
-                  setOutAnimationName(e.target.value as OutAnimation)
-                }
-              >
-                {Object.keys(outAnimation).map(key => (
-                  <option key={key} value={key}>
-                    {key}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className={styles.element}>
-              <label className={styles.label} htmlFor="inBodyAnimation">
-                In body animation
-              </label>
-              <select
-                name="inBodyAnimation"
-                className={styles.select}
-                value={inBodyAnimationName}
-                onChange={e =>
-                  setInBodyAnimationName(e.target.value as InBodyAnimation)
-                }
-              >
-                {Object.keys(inBodyAnimation).map(key => (
-                  <option key={key} value={key}>
-                    {key}
-                  </option>
-                ))}
-              </select>
+            <div className={styles.row}>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="outAnimation">
+                  Out animation
+                </label>
+                <select
+                  name="outAnimation"
+                  className={styles.select}
+                  value={outAnimationName}
+                  onChange={e =>
+                    setOutAnimationName(e.target.value as OutAnimation)
+                  }
+                >
+                  {Object.keys(outAnimation).map(key => (
+                    <option key={key} value={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className={styles.element}>
+                <label className={styles.label} htmlFor="inBodyAnimation">
+                  In body animation
+                </label>
+                <select
+                  name="inBodyAnimation"
+                  className={styles.select}
+                  value={inBodyAnimationName}
+                  onChange={e =>
+                    setInBodyAnimationName(e.target.value as InBodyAnimation)
+                  }
+                >
+                  {Object.keys(inBodyAnimation).map(key => (
+                    <option key={key} value={key}>
+                      {key}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
           </div>
         </div>
         <div className={styles.additionalSettings}>
-          <div className={styles.row}>
-            <ToggleSwitch
-              label="hide on click"
-              value={hideOnClick}
-              onValueChange={setHideOnClick}
-            />
-            <ToggleSwitch
-              value={showProgress}
-              onValueChange={setShowProgress}
-              label="show progress"
-            />
-            <ToggleSwitch
-              value={showIcon}
-              onValueChange={setShowIcon}
-              label="show icon"
-            />
-            <ToggleSwitch
-              value={showHideButton}
-              onValueChange={setShowHideButton}
-              label="show hide button"
-            />
+          <div className={styles.twoRows}>
+            <div className={styles.row}>
+              <ToggleSwitch
+                label="hide on click"
+                value={hideOnClick}
+                onValueChange={setHideOnClick}
+              />
+              <ToggleSwitch
+                value={showProgress}
+                onValueChange={setShowProgress}
+                label="show progress"
+              />
+            </div>
+            <div className={styles.row}>
+              <ToggleSwitch
+                value={showIcon}
+                onValueChange={setShowIcon}
+                label="show icon"
+              />
+              <ToggleSwitch
+                value={showHideButton}
+                onValueChange={setShowHideButton}
+                label="show hide button"
+              />
+            </div>
           </div>
-          <div className={styles.row}>
-            <ToggleSwitch
-              value={animateBody}
-              onValueChange={setAnimateBody}
-              label="animate body"
-            />
-            <ToggleSwitch value={rtl} onValueChange={setRtl} label="rtl" />
-            <ToggleSwitch
-              value={pauseOnHover}
-              onValueChange={setPauseOnHover}
-              label="pause on hover"
-            />
-            <ToggleSwitch
-              value={dragOnMobile}
-              onValueChange={setDragOnMobile}
-              label="drag on mobile"
-            />
+          <div className={styles.twoRows}>
+            <div className={styles.row}>
+              <ToggleSwitch
+                value={animateBody}
+                onValueChange={setAnimateBody}
+                label="animate body"
+              />
+              <ToggleSwitch value={rtl} onValueChange={setRtl} label="rtl" />
+            </div>
+            <div className={styles.row}>
+              <ToggleSwitch
+                value={pauseOnHover}
+                onValueChange={setPauseOnHover}
+                label="pause on hover"
+              />
+              <ToggleSwitch
+                value={dragOnMobile}
+                onValueChange={setDragOnMobile}
+                label="drag on mobile"
+              />
+            </div>
           </div>
         </div>
 
